23a59553429daa8beefa0449d7aefd12966b7c4b
rtocastro <ricktorres@live.com>
2024-08-13 08:25:58 -0700

    added logo to entry page and linked to mainpage

--- src/pages/Entry.jsx
+++ src/pages/Entry.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import pawprotectlogo from '../assets/pawprotectlogo.png'
 
 export default function Entry() {
     return (
         <>
-this is the entry page
+            <Link to='./MainPage'><img src={pawprotectlogo} /></Link>
+            <br />
+            this is the entry page
         </>
 
 
