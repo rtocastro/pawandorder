f78b371a678348064045a4d4d759d13443e21345
rtocastro <ricktorres@live.com>
2024-08-19 23:31:15 -0700

    added enter button image and linked it on entry page

--- src/pages/Entry.jsx
+++ src/pages/Entry.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import pawprotectlogo from '../assets/pawprotectlogo.png'
+import enter from '../assets/enter.png'
 
 export default function Entry() {
     return (
         <>
             <Link to='./WeatherPage'><img src={pawprotectlogo} /></Link>
             <br />
-            this is the entry page
+            <br />
+            <Link to='./WeatherPage'><img src={enter} /></Link>
+            
         </>
 
 
